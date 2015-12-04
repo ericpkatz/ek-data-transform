38e15c170f288f113d21d81924e803009ede0c2a
Eric P. Katz <ericpkatz@gmail.com>
2015-12-04 11:29:39 -0500

    allow github

--- server/env/development.js
+++ server/env/development.js
@@ -6,6 +6,11 @@ module.exports = {
     "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
     "callbackUrl": "INSERT_TWITTER_CALLBACK_HERE"
   },
+  "GITHUB": {
+    "clientID": "27d4b598317dfc0ee69e",
+    "clientSecret": "b5beabcc6ec4306bfc2dd53fc7213e5f9a68e210",
+    "callbackUrl": "http://localhost:1337/auth/github/callback"
+  },
   "FACEBOOK": {
     "clientID": "INSERT_FACEBOOK_CLIENTID_HERE",
     "clientSecret": "INSERT_FACEBOOK_CLIENT_SECRET_HERE",
